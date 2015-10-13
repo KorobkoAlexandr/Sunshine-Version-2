a7446af0f0ca3cde551e33ae074537c26a276f1c
Alex_B <korobochek@mail.ru>
2015-10-13 15:03:19 -0700

    Alex

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-//AlexB
+//Alex
 package com.example.android.sunshine.app;
 
 import android.os.AsyncTask;
