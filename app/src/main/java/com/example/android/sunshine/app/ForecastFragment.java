188e547cf82f336d2ea0e7416c72b06876fed32a
Alex_B <korobochek@mail.ru>
2016-11-09 19:34:18 +0200

    3.04 populate detail text

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -15,6 +15,7 @@
  */
 package com.example.android.sunshine.app;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -112,7 +113,9 @@ public class ForecastFragment extends Fragment {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(getActivity(),mForecastAdapter.getItem(i), Toast.LENGTH_SHORT).show();
+                Intent detailWeather = new Intent(getActivity(), DetailActivity.class);
+                detailWeather.putExtra(Intent.EXTRA_TEXT, mForecastAdapter.getItem(i));
+                startActivity(detailWeather);
             }
         });
 
